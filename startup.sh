fe3c04834e8501999db4a3d650606e46702d519d
Nils Bühner <buehner@terrestris.de>
2018-10-08 16:31:37 +0200

    Add libs before starting the tomcat

--- startup.sh
+++ startup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+ADDITIONAL_LIBS_DIR=/opt/additional_libs/
+
+# copy additional geoserver libs before starting the tomcat
+if [ -d "$ADDITIONAL_LIBS_DIR" ]; then
+    cp $ADDITIONAL_LIBS_DIR/*.jar $CATALINA_HOME/webapps/geoserver/WEB-INF/lib/
+fi
+
 # start the tomcat
 $CATALINA_HOME/bin/catalina.sh run
